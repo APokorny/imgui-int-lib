4c251f8fae7bbef7799a965aee370fddf9d6ac62
Andreas Pokorny <andreas.pokorny@gmail.com>
2021-12-27 15:16:28 +0100

    add most basic node example

--- examples/node-example.cpp
+++ examples/node-example.cpp
@@ -4,18 +4,41 @@
 #include "imgui/ui.h"
 #include "imgui/context.h"
 #include "imgui/default_style.h"
-#include "imgui/widgets.h"
+#include <imgui/widgets.h>
+#include <imgui_node_editor.h>
 
+namespace ed = ax::NodeEditor;
 std::unique_ptr<imgui::Context> ui_context;
 int                             main()
 {
-    ui_context = imgui::create_ui(
-        [clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f)](imgui::Context*) mutable {
-            // 1. Show the big demo window (Most of the sample code is in
-            // ImGui::ShowDemoWindow()! You can browse its code to learn more
-            // about Dear ImGui!).
+    ed::Config config;
+    config.SettingsFile = "node-ed-example.json";
+    ui_context          = imgui::create_ui(
+                 [clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f), m_Context = ed::CreateEditor(&config)](imgui::Context*) mutable
+                 {
+            ImGui::Text("FPS: 2323s)");
 
-        },
-        std::make_unique<imgui::DefaultStyle>(), "ImGui Example", 1200, 800);
+            ImGui::Separator();
+
+            ed::SetCurrentEditor(m_Context);
+            ed::Begin("My Editor", ImVec2(0.0, 0.0f));
+            int uniqueId = 1;
+            // Start drawing nodes.
+            ed::BeginNode(uniqueId++);
+            ImGui::Text("Node A");
+            ed::BeginPin(uniqueId++, ed::PinKind::Input);
+            ImGui::Text("-> In");
+            ed::EndPin();
+            ImGui::SameLine();
+            ed::BeginPin(uniqueId++, ed::PinKind::Output);
+            ImGui::Text("Out ->");
+            ed::EndPin();
+            ed::EndNode();
+            ed::End();
+            ed::SetCurrentEditor(nullptr);
+
+            ImGui::ShowMetricsWindow();
+                 },
+                 std::make_unique<imgui::DefaultStyle>(), "ImGui Example", 1200, 800);
     ui_context->run();
 }
